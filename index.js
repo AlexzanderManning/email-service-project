5f10eabc4a3af54d1e87b6b5a3611bf7af283e68
Deonte Manning <deontemanning@Deontes-MacBook-Pro.local>
2020-09-10 08:12:34 -0400

    staging app for deplyment with heroku.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({hi: "there"});
+})
+
+//Enviormental Variables again.
+// Great example of conditional variable assignment.
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}.`));
